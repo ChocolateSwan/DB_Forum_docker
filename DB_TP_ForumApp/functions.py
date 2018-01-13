a0bf067e1307ef5901ef0114b01339a5306861e0
ChocolateSwan <olyasur1996@gmale.com>
2018-01-13 15:56:12 +0300

    functions

--- DB_TP_ForumApp/functions.py
+++ DB_TP_ForumApp/functions.py
@@ -1,10 +1,14 @@
 async def update_users_per_forum (connection, forum_id, users_id):
-    if type(users_id) is not 'list':
+    if type(users_id) != type([]):
         users_id = [users_id]
-    await connection.execute('''
+    elif len(users_id) == 0:
+        return
+    
+    for uid in set(users_id):
+        await connection.execute('''
                            INSERT INTO users_per_forum (forum_id, user_id)
-                           SELECT $1, t.user_id
-                           FROM unnest($2::int[]) t(user_id)
+                           VALUES($1, $2)
                            ON CONFLICT DO NOTHING
-                           ''', forum_id, users_id)
+                           ''', forum_id, uid)
+
 
